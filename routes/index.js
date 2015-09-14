706a11fef967b6f0f834239f781b03b0dc3784c9
长空 <897103652@qq.com>
2015-09-14 14:34:17 +0800

    first

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '围住神经猫' });
+});
+
+module.exports = router;
