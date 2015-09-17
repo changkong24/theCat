021f99060c26d76253d1f44fe3f1ec8fe5f2f0c1
长空 <897103652@qq.com>
2015-09-17 11:21:07 +0800

    暂停中。。。。
    优先算法没想通。

--- public/js/MinPath.js
+++ public/js/MinPath.js
@@ -20,17 +20,82 @@ define(["Module"],function(Module){
 	 * 获取下一步可行的路径
 	 * @return {[type]} [description]
 	 */
-	_p.getNext = function(){
-		if(this._x == 0 || this._y == 0){
-			return;//已经到达顶点
+	_p.getNext = function(x,y){
+		var avilable = [];//可用位置
+		var cells = this._module.getCells();
+		if(x == 0 || y == 0){
+			return false;//已经到达顶点
 		}
 		//周围六个位置
-		if(this._x % 2 == 0){
+		if(x % 2 == 0){
 			//偶数行 六个位置是(x-1,y-1),(x,y-1),(x-1,y),(x+1,y),(x-1,y+1),(x,y+1)
-			
+			if(x - 1 >= 0 ){
+				if(this._module.getDataVal(x-1,y) == Module.EMPTY){
+					//可用点位
+					avilable.push({x:x-1,y:y});
+				}
+				if(y - 1 >= 0){
+					if(this._module.getDataVal(x - 1,y - 1) == Module.EMPTY){
+						avilable.push({x:x-1,y:y-1});
+					}
+				}
+				if(y + 1 < cells){
+					if(this._module.getDataVal(x - 1,y + 1) == Module.EMPTY){
+						avilable.push({x:x-1,y:y+1});
+					}
+				}
+			}
+			if(y - 1 >= 0){
+				if(this._module.getDataVal(x,y - 1) == Module.EMPTY){
+					avilable.push({x:x,y:y-1});
+				}
+			}
+			if(x + 1 < cells){
+				if(this._module.getDataVal(x+1,y) == Module.EMPTY){
+					avilable.push({x:x+1,y:y});
+				}
+			}
+			if(y + 1 < cells){
+				if(this._module.getDataVal(x,y + 1) == Module.EMPTY){
+					avilable.push({x:x,y:y+1});
+				}
+			}
 		}
 		else{
 			//奇数行 六个位置是 (x,y-1),(x+1,y-1),(x-1,y),(x+1,y),(x,y+1),(x+1,y+1)
+			if(x + 1 < cells ){
+				if(this._module.getDataVal(x + 1,y) == Module.EMPTY){
+					//可用点位
+					avilable.push({x:x + 1,y:y});
+				}
+				if(y - 1 >= 0){
+					if(this._module.getDataVal(x + 1,y - 1) == Module.EMPTY){
+						avilable.push({x:x + 1,y:y-1});
+					}
+				}
+				if(y + 1 < cells){
+					if(this._module.getDataVal(x + 1,y + 1) == Module.EMPTY){
+						avilable.push({x:x + 1,y:y+1});
+					}
+				}
+			}
+			if(y - 1 >= 0){
+				if(this._module.getDataVal(x,y - 1) == Module.EMPTY){
+					avilable.push({x:x,y:y-1});
+				}
+			}
+			if(x + 1 < cells){
+				if(this._module.getDataVal(x+1,y) == Module.EMPTY){
+					avilable.push({x:x+1,y:y});
+				}
+			}
+			if(y + 1 < cells){
+				if(this._module.getDataVal(x,y + 1) == Module.EMPTY){
+					avilable.push({x:x,y:y+1});
+				}
+			}
 		}
+		return avilable;
 	}
+
 })
